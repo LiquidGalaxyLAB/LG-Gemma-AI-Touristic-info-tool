e462611072ffd85d6d32af8dfc2a54df74bc9eae
Mahy02 <Mahinouralaa2002@gmail.com>
2024-07-24 12:24:20 +0300

    chore: build gradle settings for aab release

--- ai_touristic_info_tool/lib/helpers/settings_shared_pref.dart
+++ ai_touristic_info_tool/lib/helpers/settings_shared_pref.dart
@@ -1,3 +1,7 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 ///`LgConnectionSharedPref` to presist data in the app locally
@@ -7,12 +11,27 @@ class SettingsSharedPref {
   static const String _keyTheme = 'theme';
   static const String _keyLanguage = 'lang';
   static const String _keyTitleFont = 'titleFont';
-  static const String _keyAccentTheme= 'accentTheme';
-
- 
+  static const String _keyAccentTheme = 'accentTheme';
 
   /// Initializes the SharedPreferences instance for local data storage.
-  static Future init() async => _prefs = await SharedPreferences.getInstance();
+  // static Future init() async => _prefs = await SharedPreferences.getInstance();
+  static Future init() async {
+    _prefs = await SharedPreferences.getInstance();
+
+    // Set default values if they are not set
+    if (_prefs?.getString(_keyTheme) == null) {
+      await setTheme('default');
+    }
+    // if (_prefs?.getString(_keyLanguage) == null) {
+    //   await setLanguage(_defaultLanguage);
+    // }
+    if (_prefs?.getDouble(_keyTitleFont) == null) {
+      await setTitleFont(40);
+    }
+    if (_prefs?.getString(_keyAccentTheme) == null) {
+      await setAccentTheme('556EA5');
+    }
+  }
 
   // Setters
 
@@ -21,29 +40,28 @@ class SettingsSharedPref {
 
   static Future<void> setAccentTheme(String accentTheme) async =>
       await _prefs?.setString(_keyAccentTheme, accentTheme);
-  
+
   static Future<void> setLanguage(String lang) async =>
       await _prefs?.setString(_keyLanguage, lang);
-  
+
   static Future<void> setTitleFont(double titleFont) async =>
       await _prefs?.setDouble(_keyTitleFont, titleFont);
 
-
-///getters
+  ///getters
 
   static String? getTheme() => _prefs?.getString(_keyTheme);
   static String? getLanguage() => _prefs?.getString(_keyLanguage);
   static double? getTitleFont() => _prefs?.getDouble(_keyTitleFont);
 
   static String? getAccentTheme() => _prefs?.getString(_keyAccentTheme);
-  
 
   // Removers
 
   static Future<void> removeTheme() async => await _prefs?.remove(_keyTheme);
-  static Future<void> removeLanguage() async => await _prefs?.remove(_keyLanguage);
-  static Future<void> removeTitleFont() async => await _prefs?.remove(_keyTitleFont);
-  static Future<void> removeAccentTheme() async => await _prefs?.remove(_keyAccentTheme);
-
-
+  static Future<void> removeLanguage() async =>
+      await _prefs?.remove(_keyLanguage);
+  static Future<void> removeTitleFont() async =>
+      await _prefs?.remove(_keyTitleFont);
+  static Future<void> removeAccentTheme() async =>
+      await _prefs?.remove(_keyAccentTheme);
 }
